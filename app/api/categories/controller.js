a2e01ce1bd4259f4bb01d227cc67b9f759b75b7c
Vishal Gautam <ivishalgautamm@gmail.com>
2024-07-10 16:58:47 +0530

    added seo data in categories

--- app/api/categories/controller.js
+++ app/api/categories/controller.js
@@ -27,9 +27,6 @@ const create = async (req, res) => {
 
 const updateById = async (req, res) => {
   try {
-    let slug = slugify(req.body.name, { lower: true });
-    req.body.slug = slug;
-
     const record = await table.CategoryModel.getById(req, req.params.id);
 
     if (!record) {
@@ -58,9 +55,6 @@ const updateById = async (req, res) => {
 
 const getBySlug = async (req, res) => {
   try {
-    let slug = slugify(req.body.title, { lower: true });
-    req.body.slug = slug;
-
     const record = await table.CategoryModel.getBySlug(req, req.params.slug);
 
     if (!record) {
